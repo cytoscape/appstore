e5e3be9ff72c086e947d2820a3696383c8ccda4d
sarthak-srivastava <sarth1597@gmail.com>
2018-07-17 17:49:02 +0000

    fullname downloads votes and latest_release_date added to indices

--- apps/search_indexes.py
+++ apps/search_indexes.py
@@ -4,11 +4,18 @@ from apps.models import App
 class AppIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.EdgeNgramField(document = True, use_template = True)
     name = indexes.CharField(model_attr = 'name')
+    fullname = indexes.CharField(model_attr='fullname',null=True)
     description = indexes.CharField(model_attr = 'description',null=True)
     details = indexes.CharField(model_attr = 'details',null=True)
-    has_releases = indexes.BooleanField(model_attr='has_releases')
+    has_releases = indexes.BooleanField(model_attr='has_releases',null = True)
     tags = indexes.MultiValueField(model_attr = 'tags',null=True)
     authors = indexes.MultiValueField(model_attr = 'authors',null=True)
+    downloads = indexes.IntegerField(model_attr = 'downloads',null = True)
+    votes = indexes.IntegerField(model_attr = 'votes',null = True)
+    latest_release_date = indexes.DateField(model_attr = 'latest_release_date',null= True)
+
+    
+
     def get_model(self):
         return App
     def prepare_authors(self, obj):
