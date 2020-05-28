d8c952bd310dadd34ec003ee58af14c5a5980689
Chris Churas <churas.camera@gmail.com>
2020-05-28 13:10:53 -0700

    to support 1.9.13 of Django replaced syncdb with makemigration calls. Also disabled 000-default.conf apache config and set apache to also listen on 8080. Finally set password for database randomly

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,6 +8,8 @@ apt-get -y install apache2 apache2-dev apache2-utils ssl-cert wget
 apt-get -y install libapache2-mod-wsgi-py3 gcc g++
 apt-get -y install mysql-server
 apt-get -y install libjpeg8-dev
+
+# not sure if this is needed anymore
 apt-get -y install geoip-database
 
 # install miniconda3
@@ -22,9 +24,6 @@ export PATH=/opt/miniconda3/bin:$PATH
 echo "export PATH=/opt/miniconda3/bin:$PATH" >> /root/.bash_profile
 echo "export PATH=/opt/miniconda3/bin:$PATH" >> /root/.bashrc
 sudo -u vagrant echo "export PATH=/opt/miniconda3/bin:$PATH" >> /home/vagrant/.bash_profile
-
-conda install -y -c conda-forge python-xapian
-
 
 # install mod_wsgi
 pip install mod_wsgi
@@ -47,36 +46,18 @@ mod_wsgi-express install-module | egrep "^LoadModule" > /etc/apache2/mods-availa
 # enable wsgi
 a2enmod wsgi
 
-# build and install xapian
-
-# XAPIAN_VERSION="1.4.15"
-#XAPIAN_CORE="xapian-core-"
-#wget https://oligarchy.co.uk/xapian/${XAPIAN_VERSION}/${XAPIAN_CORE}${XAPIAN_VERSION}.tar.xz
-#unxz ${XAPIAN_CORE}${XAPIAN_VERSION}.tar.xz
-#tar -xf ${XAPIAN_CORE}${XAPIAN_VERSION}.tar
-#pushd ${XAPIAN_CORE}${XAPIAN_VERSION}
-#./configure
-#make
-#make install
-#popd
-#rm -rf ${XAPIAN_CORE}${XAPIAN_VERSION}*
-
 # updates shared library cache
 ldconfig
 
-# build and install xapian-bindings
-#XAPIAN_BINDINGS="xapian-bindings-"
-#wget https://oligarchy.co.uk/xapian/${XAPIAN_VERSION}/${XAPIAN_BINDINGS}${XAPIAN_VERSION}.tar.xz
-#unxz ${XAPIAN_BINDINGS}${XAPIAN_VERSION}.tar.xz
-#tar -xf ${XAPIAN_BINDINGS}${XAPIAN_VERSION}.tar
-#pushd ${XAPIAN_BINDINGS}${XAPIAN_VERSION}
-#./configure --with-python3
-#make
-#make install
-#popd
-#rm -rf ${XAPIAN_BINDINGS}${XAPIAN_VERSION}*
-
 pip install -r /vagrant/requirements.txt
+
+# create database
+mysqladmin create AppStore
+
+dbpass=`uuidgen`
+cat /vagrant/createdb.sql | sed "s/@@PASSWORD@@/$dbpass/g" > /tmp/createdb.sql
+
+mysql -u root < /tmp/createdb.sql
 
 # TODO get app working properly
 cd /var/www
@@ -85,13 +66,26 @@ cd CyAppStore
 cp -a /vagrant/* .
 mkdir logs
 mkdir media
+
+# update the database password
+sed -i "s/@@PASSWORD@@/$dbpass/g" /var/www/CyAppStore/settings.py
+
 rm /var/www/CyAppStore/appstore.http.conf
 rm /var/www/CyAppStore/appstore.include.conf
 
 cd /var/www/CyAppStore
-python manage.py syncdb --noinput
+
+python manage.py makemigrations apps --noinput
+python manage.py makemigrations backend --noinput
+python manage.py makemigrations download --noinput
+python manage.py makemigrations help --noinput
+python manage.py makemigrations search --noinput
+python manage.py makemigrations submit_app --noinput
+python manage.py makemigrations users --noinput
 python manage.py makemigrations --noinput
+
 python manage.py migrate --noinput
+python manage.py rebuild_index --noinput
 
 # fix permissions
 chown -R www-data:www-data /var/www
@@ -102,6 +96,8 @@ find /var/www -type f -exec chmod 640 {} \+
 mkdir /etc/apache2/includes
 cp /vagrant/appstore.include.conf /etc/apache2/includes/.
 cp /vagrant/appstore.http.conf /etc/apache2/sites-available/appstore.conf
+echo "Listen 8080" >> /etc/apache2/ports.conf
+a2dissite 000-default.conf
 a2ensite appstore.conf
 
 
