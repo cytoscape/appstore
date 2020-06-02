ddc032d546b0296054f8d7b9fc880eb312657419
Chris Churas <churas.camera@gmail.com>
2020-06-02 11:20:54 -0700

    on_delete must be set when using ForeignKey field in Django 2.0+ and made some style fixes

--- download/models.py
+++ download/models.py
@@ -1,35 +1,46 @@
-from django.db.models import Model, CharField, PositiveIntegerField, ForeignKey, DateField
+from django.db.models import Model, CharField, PositiveIntegerField
+from django.db.models import ForeignKey, DateField
 from apps.models import App, Release
+from django.db import models
 from util.view_util import ipaddr_long_to_str
 
+
 class Download(Model):
-    release = ForeignKey(Release, related_name='app_download_stats')
-    when    = DateField()
+    release = ForeignKey(Release, related_name='app_download_stats',
+                         on_delete=models.CASCADE)
+    when = DateField()
     ip4addr = PositiveIntegerField()
 
     def __unicode__(self):
-        return unicode(self.release) + u' ' + unicode(self.when) + u' ' + ipaddr_long_to_str(self.ip4addr)
+        return unicode(self.release) + u' ' + unicode(self.when) +\
+               u' ' + ipaddr_long_to_str(self.ip4addr)
+
 
 class ReleaseDownloadsByDate(Model):
-    release = ForeignKey(Release, null = True) # null release has total count across a given day
-    when    = DateField()
-    count   = PositiveIntegerField(default = 0)
+    release = ForeignKey(Release, on_delete=models.CASCADE,
+                         null=True)  # null release has total count across a given day
+    when = DateField()
+    count = PositiveIntegerField(default=0)
 
     def __unicode__(self):
-        return unicode(self.release) + u' ' + unicode(self.when) + u': ' + unicode(self.count)
+        return unicode(self.release) + u' ' + unicode(self.when) + u': ' +\
+               unicode(self.count)
+
 
 class GeoLoc(Model):
-    country = CharField(max_length = 2) # when region & city are empty, country contains the total
-    region  = CharField(max_length = 2,  blank = True)
-    city    = CharField(max_length = 63, blank = True)
+    country = CharField(max_length=2)  # when region & city are empty, country contains the total
+    region = CharField(max_length=2,  blank=True)
+    city = CharField(max_length=63, blank=True)
 
     def __unicode__(self):
         return self.country + u' ' + self.region + u' ' + self.city
 
+
 class AppDownloadsByGeoLoc(Model):
-    app    = ForeignKey(App, null = True) # null app has total count across a given geoloc
-    geoloc = ForeignKey(GeoLoc)
-    count  = PositiveIntegerField(default = 0)
+    app = ForeignKey(App, null=True, on_delete=models.CASCADE)  # null app has total count across a given geoloc
+    geoloc = ForeignKey(GeoLoc, on_delete=models.CASCADE)
+    count = PositiveIntegerField(default=0)
 
     def __unicode__(self):
-        return unicode(self.app) + u' ' + unicode(self.geoloc) + u': ' + unicode(self.count)
+        return unicode(self.app) + u' ' + unicode(self.geoloc) + u': ' +\
+               unicode(self.count)
