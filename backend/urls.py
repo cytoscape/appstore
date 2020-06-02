1eff64ad8532ae615b422936ff8092271381bfd9
Chris Churas <churas.camera@gmail.com>
2020-06-02 15:55:14 -0700

    all might shadow a builtin python name so changed function name

--- backend/urls.py
+++ backend/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 
-from backend.views import all
+from backend.views import all_apps_func
 
 urlpatterns = [
-    url(r'^all_apps$', all),
+    url(r'^all_apps$', all_apps_func),
 ]
