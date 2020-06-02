b6e27fe73d47731d530793a80db3efb3f0ec2c54
Chris Churas <churas.camera@gmail.com>
2020-06-02 15:55:59 -0700

    renamed all function to all_apps_func cause all may shadow a python variable. also fixed style

--- backend/views.py
+++ backend/views.py
@@ -1,13 +1,32 @@
 from apps.models import App
 from util.view_util import obj_to_dict, iter_to_dict, json_response
 
-def _app_to_obj(app):
-	result = obj_to_dict(app, ('fullname', 'description', 'icon_url',  'page_url', 'downloads', 'stars_percentage', 'votes', 'citation'))
-	result['tags'] = [tag.fullname for tag in app.tags.all()]
-	result['releases'] = iter_to_dict(app.releases, ('version', 'created_iso', 'release_download_url', 'works_with', 'hexchecksum', 'notes'))
-	return result
 
-def all(request):
-	all_apps = App.objects.filter(active = True)
-	all_apps_fmt = [_app_to_obj(app) for app in all_apps if app.has_releases]
-	return json_response(all_apps_fmt)
+def _app_to_obj(app):
+    """
+    Create
+    :param app:
+    :return:
+    :rtype: dict
+    """
+    result = obj_to_dict(app, ('fullname', 'description', 'icon_url',
+                               'page_url', 'downloads', 'stars_percentage',
+                               'votes', 'citation'))
+    result['tags'] = [tag.fullname for tag in app.tags.all()]
+    result['releases'] = iter_to_dict(app.releases, ('version', 'created_iso',
+                                                     'release_download_url',
+                                                     'works_with',
+                                                     'hexchecksum', 'notes'))
+    return result
+
+
+def all_apps_func(request):
+    """
+    Gets all App objects that are active with releases
+
+    :param request:
+    :return: active App objects with releases in json format
+    """
+    all_apps = App.objects.filter(active=True)
+    all_apps_fmt = [_app_to_obj(app) for app in all_apps if app.has_releases]
+    return json_response(all_apps_fmt)
