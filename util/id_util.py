4f5cb988f6d2c0137d53e6cac79b209ad845cf8d
Chris Churas <churas.camera@gmail.com>
2021-01-24 16:03:51 -0800

    added some documentation

--- util/id_util.py
+++ util/id_util.py
@@ -1,5 +1,17 @@
 import re
 
 _non_alphanum_re = re.compile('[\W_]+')
+
+
 def fullname_to_name(fullname):
+    """
+    Creates str from 'fullname' where all non word
+    characters are removed. Any remaining characters
+    are lower cased
+
+    :param fullname: Input str
+    :type fullname: str
+    :return: 'fullname' lowercased with non word characters removed
+    :rtype: str
+    """
     return _non_alphanum_re.sub('', fullname).lower()
