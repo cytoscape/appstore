3fcd0e8e0eede1165f4c5904021f9eb6a8cb5d20
Chris Churas <churas.camera@gmail.com>
2021-01-24 16:05:14 -0800

    added documentation and reworked if statement in _get_value method

--- submit_app/pomparse.py
+++ submit_app/pomparse.py
@@ -2,20 +2,62 @@ import xml.etree.ElementTree as ET
 import re
 
 _NsTagRE = re.compile(r'\{([^\}]+)\}(.+)')
+"""
+Regex used to parse namespace
+"""
+
+
+PomAttrNames = ('groupId', 'artifactId', 'version')
+"""
+Attribute names to parse from pom file
+used by parse_pom() function
+"""
+
+
 def _parse_ns_tag(ns_tag):
+    """
+    Assumes input is {text...}xxx and returns
+    the content between the curly braces ie text...
+
+    :param ns_tag:
+    :return: (content between curly braces) or (None, None)
+             if no group of curly braces are found
+    :rtype: tuple
+    """
     m = _NsTagRE.match(ns_tag)
     return m.groups() if m else (None, None)
 
+
 def _mk_tag(ns, tag):
+    """
+    If 'ns' is not None this function puts curly braces
+    around it and appends value of 'tag' otherwise value of
+    'tag' is returned
+
+    :param ns: namespace
+    :param tag:
+    :return: {ns}tag or tag if ns is None
+    :rtype: str
+    """
     return '{%s}%s' % (ns, tag) if ns else tag
+
 
 def _get_value(root, ns, tag):
     elem = root.find(_mk_tag(ns, tag))
-    return elem.text if elem != None else None
+    if elem is None:
+        return None
+    return elem.text
 
-PomAttrNames = ('groupId', 'artifactId', 'version')
 
 def parse_pom(inputfile):
+    """
+    Parses pom file passed in
+
+    :param inputfile:
+    :return: If found, 'groupId', 'artifactId', and 'version' will be
+             added to dict. if none are found and empty dict is returned
+    :rtype: dict
+    """
     try:
         tree = ET.parse(inputfile)
     except:
