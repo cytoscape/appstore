0be80bce485ee28fe8b972cbe6e6c7da70feb963
kvn879 <kzhu079@gmail.com>
2026-07-24 17:36:12 -0600

    support for bundles

--- web_bundle_storage/mcode2/0.1.0/@mf-types.d.ts
+++
@@ -1,3 +0,0 @@
-
-    export type RemoteKeys = 'REMOTE_ALIAS_IDENTIFIER/AppConfig';
-    type PackageType<T> = T extends 'REMOTE_ALIAS_IDENTIFIER/AppConfig' ? typeof import('REMOTE_ALIAS_IDENTIFIER/AppConfig') :any;
