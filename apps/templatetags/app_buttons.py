caa15a47276e70418c7846b5aee99680869a4d99
kvn879 <kzhu079@gmail.com>
2026-07-27 12:50:48 -0600

    fix search 2.1

--- apps/templatetags/app_buttons.py
+++ apps/templatetags/app_buttons.py
@@ -34,7 +34,7 @@ def app_buttons(apps):
 
 
 @register.inclusion_tag('list_of_apps_search.html')
-def list_of_apps_search(apps, platform , include_relevancy = False):
+def list_of_apps_search(apps, platform=None, include_relevancy = False):
     apps = filter(lambda a: hasattr(a.object, 'has_releases'), apps)
     apps_with_releases = filter(lambda a: a.object.has_releases, apps)
     apps_without_releases = filter(lambda a: not a.object.has_releases, apps)
